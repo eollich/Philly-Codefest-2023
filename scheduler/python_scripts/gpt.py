58173c11c28f08cdc750817b001c06e29d77c96f
erick <eollich@gmail.com>
2023-03-11 22:27:01 -0500

    gpt prompt updated

--- scheduler/python_scripts/gpt.py
+++ scheduler/python_scripts/gpt.py
@@ -12,3 +12,26 @@ def query(query_string):
     )
     print(x)
     return x
+
+
+
+def query2(query_string):
+    ai_key = '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'
+    openai.api_key = ai_key
+    
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=query_string,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0,
+    )
+    
+    res = response.choices[0].text
+    
+    print(res)
+    return res
+
+def generateQueryString(title, description, role):
+    return f'Generate an outline with the steps necessary to prepare for a meeting titled "{title}" with a description "{description}" for a "{role}". Provide output in only bullet points'
